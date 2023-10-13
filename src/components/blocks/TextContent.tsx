8229fb650bfeab0c2afb2881c55767569ec1c778
WEBzaytsev <studio@webzaytsev.ru>
2023-10-13 23:43:50 +0300

    feat: create Text Content block

---
+++ src/components/blocks/TextContent.tsx
@@ -0,0 +1,44 @@
+import React, {useCallback, useRef} from "react";
+import {createReactEditorJS} from "react-editor-js";
+import LinkTool from "@editorjs/link";
+import Header from "@editorjs/header";
+import {EditorCore, TextContent as TextContentType} from "../../types/blocks/TextContent.ts";
+import {useEditorContentContext} from "../providers/EditorContentProvider.tsx";
+
+const ReactEditorJS = createReactEditorJS();
+
+interface Props {
+    block: TextContentType;
+}
+
+const TextContent = (props: Props) => {
+    const {block} = props;
+    const {editorContent, updateEditorContentItem} = useEditorContentContext();
+    const editorCore = useRef<EditorCore | null>(null);
+
+    const isCurrentBlockRefSet = (): boolean =>
+        Boolean(editorContent.find((item) => item.id === block.id)?.editorRef);
+
+    const handleInitialize = useCallback((instance: EditorCore | null): void => {
+        editorCore.current = instance;
+
+        if (isCurrentBlockRefSet()) return;
+
+        updateEditorContentItem({...block, editorRef: editorCore});
+    }, []);
+
+    const EDITOR_JS_TOOLS = {
+        linkTool: LinkTool,
+        header: Header,
+    }
+
+    return (
+        <ReactEditorJS
+            tools={EDITOR_JS_TOOLS}
+            defaultValue={block.content}
+            onInitialize={handleInitialize}
+        />
+    )
+}
+
+export default TextContent;
