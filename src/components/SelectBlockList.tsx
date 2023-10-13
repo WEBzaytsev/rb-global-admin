d3a07572de2984df287c8d7c1f9df7b3a1c5b5ba
WEBzaytsev <studio@webzaytsev.ru>
2023-10-13 23:43:40 +0300

    feat: create blocks selection

---
+++ src/components/SelectBlockList.tsx
@@ -0,0 +1,77 @@
+import {BlockTypes} from "../types/BlockTypes.ts";
+import {TextContent} from "../types/blocks/TextContent.ts";
+import {useEditorContentContext} from "./providers/EditorContentProvider.tsx";
+
+interface Props {
+    hideBlocksList: () => void;
+}
+
+const blocks = Object.values(BlockTypes).map((item) => ({
+    type: item,
+    icon: ''
+}));
+
+const SelectBlockList = (props: Props) => {
+    const {hideBlocksList} = props;
+    const {editorContent, addEditorContentItem} = useEditorContentContext();
+
+    const select = (type: BlockTypes): void => {
+        switch (type) {
+            case BlockTypes.TEXT_CONTENT:
+                addEditorContentItem({
+                    id: editorContent.length + 1,
+                    blockType: type,
+                    html: null,
+                    editorRef: null,
+                    content: undefined,
+                });
+                break;
+        }
+
+        hideBlocksList();
+    }
+
+    const formattedBlockTitle = (typeTitle: string): string => {
+        return typeTitle
+            .replace(/([a-z])([A-Z])/g, '$1 $2')
+            .replace(/^./, (str) => str.toUpperCase());
+    }
+
+    return (
+        <div
+            className=""
+            style={{
+                display: "flex",
+                position: 'absolute',
+                alignItems: 'center',
+                justifyContent: 'center',
+                columnGap: '10px',
+                backgroundColor: '#fff',
+                borderRadius: '6px',
+                border: '1px solid #999',
+                padding: '5px 10px',
+                top: '110%',
+                left: '50%',
+                transform: 'translateX(-50%)'
+            }}
+        >
+            {blocks.map((block) => (
+                <div
+                    key={block.type}
+                    style={{
+                        padding: '5px 10px',
+                        borderRadius: '6px',
+                        border: '1px solid #999',
+                        cursor: "pointer",
+                        whiteSpace: "nowrap"
+                    }}
+                    onClick={() => select(block.type)}
+                >
+                    {formattedBlockTitle(block.type)}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default SelectBlockList;
