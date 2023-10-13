f757c59eed6e5be89d8fb96facec0cf6e39a63a5
WEBZaytsev <nikita@zaitsv.dev>
2023-10-13 23:40:17 +0300

    refactor: edit env variables

--- src/types/main.d.ts
+++ src/types/main.d.ts
@@ -1,6 +1,10 @@
 declare module "@/*";
 declare module "@editorjs/link";
 interface ImportMetaEnv extends Readonly<Record<string, string>> {
+  readonly VITE_API_PROTOCOL: string;
+  readonly VITE_API_HOSTNAME: string;
+  readonly VITE_API_PORT: string;
+  readonly VITE_API_PATHNAME: string;
   readonly VITE_APP_TITLE: string;
 }
 
