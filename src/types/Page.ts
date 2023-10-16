845cfca5f32b511d0897659354241e36448523ca
WEBzaytsev <studio@webzaytsev.ru>
2023-10-16 14:23:24 +0300

    refactor(types): separate TextContent to DBTextContentBlock and ClientTextContentBlock

--- src/types/Page.ts
+++ src/types/Page.ts
@@ -1,12 +1,12 @@
 import {PostTypes} from "./PostTypes.ts";
-import {TextContent} from "./blocks/TextContent.ts";
+import {DBTextContentBlock} from "./blocks/TextContent.ts";
 
 export interface Page {
     readonly id: number;
     readonly postType: PostTypes.PAGE;
     title: string;
     slug: string;
-    content: (TextContent)[];
+    content: (DBTextContentBlock)[];
     createdBy: string;
     updatedBy: string | null;
     readonly createdAt: string;
